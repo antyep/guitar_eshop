a8abef9b2db4749b90345d0e7ca1e52ba3b3a308
kulde <ku1de@icloud.com>
2024-07-21 22:25:51 +0200

    feat: final props added to display name, image, description and prices of the guitars

--- src/components/Guitar.jsx
+++ src/components/Guitar.jsx
@@ -1,24 +1,20 @@
 export default function Guitar({ guitar }) {
-  console.log(guitar);
+  const { id, name, image, description, price } = guitar;
+
   return (
     <>
       <div className="col-md-6 col-lg-4 my-4 row align-items-center">
         <div className="col-4">
           <img
             className="img-fluid"
-            src="./public/img/guitarra_01.jpg"
+            src={`/img/${image}.jpg`}
             alt="Image guitarra"
           />
         </div>
         <div className="col-8">
-          <h3 className="text-black fs-4 fw-bold text-uppercase">
-            {guitar.name}
-          </h3>
-          <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit quae
-            labore odit magnam in autem nesciunt, amet deserunt
-          </p>
-          <p className="fw-black text-primary fs-3">€299</p>
+          <h3 className="text-black fs-4 fw-bold text-uppercase">{name}</h3>
+          <p>{description}</p>
+          <p className="fw-black text-primary fs-3">€{price}</p>
           <button type="button" className="btn btn-dark w-100">
             Add to cart
           </button>
