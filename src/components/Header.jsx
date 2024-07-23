936b4844ae3cbbe7cc33a3e5068821c9d79e3d2a
antyep <antyep6@gmail.com>
2024-07-23 13:52:15 +0200

    feat(cart): add a message when cart has no elements in it

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -22,54 +22,61 @@ export default function Header({ cart }) {
                 />
 
                 <div id="cart" className="bg-white p-3">
-                  <p className="text-center">Cart is empty</p>
-                  <table className="w-100 table">
-                    <thead>
-                      <tr>
-                        <th>Image</th>
-                        <th>Name</th>
-                        <th>Price</th>
-                        <th>Quantity</th>
-                        <th></th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      {cart.map((guitar) => (
-                        <tr key={guitar.id}>
-                          {" "}
-                          <td>
-                            <img
-                              className="img-fluid"
-                              src={`/img/${guitar.image}.jpg`}
-                              alt="Image guitarra"
-                            />
-                          </td>
-                          <td>{guitar.name}</td>
-                          <td className="fw-bold">{guitar.price}</td>
-                          <td className="flex align-items-start gap-4">
-                            <button type="button" className="btn btn-dark">
-                              -
-                            </button>
-                            {guitar.quantity}
-                            <button type="button" className="btn btn-dark">
-                              +
-                            </button>
-                          </td>
-                          <td>
-                            <button className="btn btn-danger" type="button">
-                              X
-                            </button>
-                          </td>
-                        </tr>
-                      ))}
-                    </tbody>
-                  </table>
-                  <p className="text-end">
-                    Total: <span className="fw-bold">€899</span>
-                  </p>
-                  <button className="btn btn-dark w-100 mt-3 p-2">
-                    Empty cart
-                  </button>
+                  {cart.length === 0 ? (
+                    <p className="text-center">Cart is empty</p>
+                  ) : (
+                    <>
+                      <table className="w-100 table">
+                        <thead>
+                          <tr>
+                            <th>Image</th>
+                            <th>Name</th>
+                            <th>Price</th>
+                            <th>Quantity</th>
+                            <th></th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          {cart.map((guitar) => (
+                            <tr key={guitar.id}>
+                              <td>
+                                <img
+                                  className="img-fluid"
+                                  src={`/img/${guitar.image}.jpg`}
+                                  alt="Image guitarra"
+                                />
+                              </td>
+                              <td>{guitar.name}</td>
+                              <td className="fw-bold">{guitar.price}</td>
+                              <td className="flex align-items-start gap-4">
+                                <button type="button" className="btn btn-dark">
+                                  -
+                                </button>
+                                {guitar.quantity}
+                                <button type="button" className="btn btn-dark">
+                                  +
+                                </button>
+                              </td>
+                              <td>
+                                <button
+                                  className="btn btn-danger"
+                                  type="button"
+                                >
+                                  X
+                                </button>
+                              </td>
+                            </tr>
+                          ))}
+                        </tbody>
+                      </table>
+                      <p className="text-end">
+                        Total: <span className="fw-bold">€899</span>
+                      </p>
+                      <button className="btn btn-dark w-100 mt-3 p-2">
+                        Empty cart
+                      </button>
+                    </>
+                  )}
                 </div>
               </div>
             </nav>
